773c0cd9a7a4e4113f1d0820fdc6b9df280d987e
eaguirreabonzaSM2025 <abonzaernesto@gmail.com>
2025-07-14 21:08:49 -0400

    gradebook.js

---
+++ gradebook.js
@@ -0,0 +1,14 @@
+function fetchGradeData() {
+    console.log("fetchGradeData called");
+    // This function will fetch data from the database in a future assignment
+}
+
+//Populate the table with grade data
+function populateGradebook(data) {
+    console.log("populateGradebook called with data:", data);
+    // This function will populate the gradebook table with the provided data
+}
+
+// Call the stubs for testing (demonstrate workflow)
+fetchGradeData();
+populateGradebook([]);
