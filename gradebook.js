a8804a254864a9daadb3444c1dca025c87150978
eaguirreabonza1SP2025 <ernestoabonza@gmail.com>
2025-07-20 19:44:23 -0400

    Update gradebook.js

--- gradebook.js
+++ gradebook.js
@@ -10,7 +10,7 @@ function fetchGradeData() {
     xhr.onreadystatechange = function () {
         if (xhr.readyState === xhr.DONE) {
             if (xhr.status === 200) {
-               console.error('Could not get grades.
+               console.error('Could not get grades.');
 		 Status: ${xhr.status}');
 	    }
 	    populateGradebook(JSON.parse(xhr.responseText));
