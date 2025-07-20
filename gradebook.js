f88a20d4e473290c44e5271084df28c61793d277
eaguirreabonza1SP2025 <ernestoabonza@gmail.com>
2025-07-20 18:14:38 -0400

    Update gradebook.js

--- gradebook.js
+++ gradebook.js
@@ -4,18 +4,46 @@ js
 
 async function fetchGradeData() {
   console.log("Getting grade data...");
-  return [];
+
+  let xhr = new XMLHttpRequest();
+  let apiRoute = "/api/grades";
+
+  xhr.onreadystatechange = function () {
+	if (xhr.readyState === xhr.DONE) {
+	    if (xhr.status !== 200){
+		console.error('Could not get grades. Status: $(xhr.status}');
+		return;
+	    }
+
+	    const data = JSON.parse(xhr.responseText);
+	    populateGradebook(data);
+	}
+ }.bind(this);
+
+ xhr.open("get", apiRoute, true);
+ xhr.send();
+
+}
+ function populateGradebook(data) {
+    console.log("Filling table with:", data);
+    const tableBody = document.getElementById("gradebook-body");
+    tableBody.innerHTML = "";
+
+    data.forEach(student => {
+        const row = document.createElement("tr");
+
+        const nameCell = document.createElement("td");
+        nameCell.textContent = student.name;
+
+        const gradeCell = document.createElement("td");
+        gradeCell.textContent = student.grade;
+
+        row.appendChild(nameCell);
+        row.appendChild(gradeCell);
+        tableBody.appendChild(row);
+    });
 }
 
-function populateGradebook(data) {
-  console.log("Filling table with:", data);
-  const tableBody = document.getElementById("gradebook-body");
-  tableBody.innerHTML = "";
-
-  // adding rows
-}
-
-document.addEventListener("DOMContentLoaded", async function() {
-  const data = await fetchGradeData();
-  populateGradebook(data);
+ document.addEventListener("DOMContentLoaded", function () {
+    fetchGradeData();
 });
